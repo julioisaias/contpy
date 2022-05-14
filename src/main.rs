c1cbc7726015a9bdf827203b977849659311daaf
julioisaias <julioisaias@gmail.com>
2022-05-14 16:45:52 -0400

    Some Issues

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::{ thread, time };
+use std::{ thread, time, collections::HashSet };
 use csv::{ Error, ReaderBuilder };
 use serde::Deserialize;
+use std::iter::Iterator;
 
 #[derive(Debug, Deserialize)]
 struct Record {
@@ -27,15 +28,43 @@ struct Record {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    let v = get_data().await?;
+    //let v = get_data().await?;
 
-    println!("{:#?}", v);
+
+    let v1 = vec![1,2,3];
+    let v2 = vec![3,2,1];
+
+
+    let r = is_new(&v1,&v2);
+
+    if r {
+        let n = get_new(v1, v2);
+        println!("HAY NUEVOS: {:#?}", n);
+    }else{
+        println!("NO HAY NUEVOS");
+    }
+
+    
+    //println!("{:#?}", v);
 
 //    let ten_millis = time::Duration::from_millis(2000);
 //    let now = time::Instant::now();
 
     Ok(())
 }
+
+
+fn get_new(vec1: Vec<usize>, vec2: Vec<usize>)-> Vec<usize> {
+    let item_set: HashSet<_> = vec1.iter().collect();
+    let difference: Vec<_> = vec2.into_iter().filter(|item| !item_set.contains(item)).collect();
+    return difference;
+}
+
+
+fn is_new(vec1: &Vec<usize>, vec2: &Vec<usize>)-> bool {
+    return !vec1.iter().eq(vec2.iter())
+}
+
 
 
 
