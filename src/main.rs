9ec37fabea3f9459f6ad23975d20775debc5736c
JULIO ISAIAS IGLESIA BERNAL <ter00628@itaipudigital.onmicrosoft.com>
2022-05-18 09:16:42 -0400

    Code

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,6 @@ struct Record {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     let mut v1: Vec<usize> = get_data().await?;
     let verb: time::Duration = time::Duration::from_secs(600); // Every 10 minutes (600 secs)
 
@@ -33,36 +32,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 
 fn get_compare(mut vec1: Vec<usize>, mut vec2: Vec<usize>)-> (bool, Vec<usize>) {
-
     vec1.sort(); // SORT
     vec2.sort();
-
     let w1: HashSet<usize> = vec1.into_iter().collect(); // Remove duplicates
     let w2: HashSet<usize> = vec2.into_iter().collect();
     let diff: HashSet<_> = w2.difference(&w1).cloned().collect();
-
     let vec: Vec<usize> = Vec::from_iter(diff);
     let is_diff: bool = if vec.len() > 0 { true } else { false };
-    
-    return (is_diff, vec);
+
+    (is_diff, vec)
 }
 
 
 async fn get_data() -> Result<Vec<usize>, Box<dyn std::error::Error>> {
-
     let url: &str = "https://www.contrataciones.gov.py/";
     let parameters: String = module::Params::new();
-
     let uri: String = format!("{}{}", url, parameters);
-
     let data: String = reqwest::get(uri)
     .await?
     .text()
     .await?;
-
     let mut reader: csv::Reader<&[u8]> = ReaderBuilder::new().delimiter(b';').from_reader(data.as_bytes());
     let mut current_vec: Vec<usize> = Vec::new();
-
     for record in reader.deserialize() {
         let dd: Record = record?;
         current_vec.push(dd.nro_licitacion);
